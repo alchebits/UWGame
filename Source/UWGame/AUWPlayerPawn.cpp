29a9d927c80cf4b334d4ba5bba87ac36652658d3
AlchebitsDM <alchebits@gmail.com>
2025-11-02 13:37:07 +0100

    02 simple player pawn with basic input and movement implemented

---
+++ Source/UWGame/AUWPlayerPawn.cpp
@@ -0,0 +1,30 @@
+﻿#include "AUWPlayerPawn.h"
+
+void AUWPlayerPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (PlayerInputComponent)
+	{
+		PlayerInputComponent->BindAxis("MoveForward", this, &AUWPlayerPawn::MoveForward);
+		PlayerInputComponent->BindAxis("MoveRight", this, &AUWPlayerPawn::MoveRight);
+	}
+}
+
+void AUWPlayerPawn::MoveForward(float UnUnit)
+{
+	float DeltaTimeSeconds = GetWorld()->GetDeltaSeconds();
+	float FinalDeltaForwardMovement = DeltaTimeSeconds * PlayerSpeedPerSecond * UnUnit;
+
+	FVector DeltaForward = FVector(FinalDeltaForwardMovement, 0.f, 0.f);
+	AddActorWorldOffset(DeltaForward);
+}
+
+void AUWPlayerPawn::MoveRight(float UnUnit)
+{
+	float DeltaTimeSeconds = GetWorld()->GetDeltaSeconds();
+	float FinalDeltaRightMovement = DeltaTimeSeconds * PlayerSpeedPerSecond * UnUnit;
+	
+	FVector DeltaRight = FVector(0.f, FinalDeltaRightMovement, 0.f);
+	AddActorWorldOffset(DeltaRight);
+}
---
+++ Source/UWGame/AUWPlayerPawn.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "GameFramework/Pawn.h"
+#include "AUWPlayerPawn.generated.h"
+
+UCLASS()
+class AUWPlayerPawn : public APawn
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+	
+	void MoveForward(float UnUnit);
+	void MoveRight(float UnUnit);
+
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float PlayerSpeedPerSecond = 10.f;
+};
+
